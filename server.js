4e230edbd503b9589d1b762c71199f429c346587
Hester Clapp <18hclapp@cms.tela.org.uk>
2024-07-31 20:33:42 +0100

    deposit functionality

--- server.js
+++ server.js
@@ -28,35 +28,46 @@ const server = Bun.serve({
                     console.log(teams)
                     return teams
                 })
-                return Response.json({ team });
+                return Response.json({ team }, { status: 201 });
             }
-            // if (url.pathname === "/teamdata") {
-            //     const team = await req.text()
-            //     console.log(`from team ${team}`)
-            //     readJSON("teams.json", teams => {
-            //         return Response.json(teams[team] || {})
-            //     })
-            // }
-            if (url.pathname === "/claim") {
-                const data = await req.text().then(text => JSON.parse(text))
+            if (url.pathname === "/deposit") {
+                const data = await req.text().then(JSON.parse)
+                let status, teamData
+                const teams = await Bun.file("teams.json").json()
                 if (data.team in teams) {
-                    readJSON("challenges.json", challenges => {
-                        // Close the challenge
-                        const challenge = challenges.locational.concat(challenges.nonlocational)
-                            .filter(v => v.title === data.challenge)[0]
-                        challenge.open = false
-                        Bun.write("challenges.json", JSON.stringify(challenges))
+                    teamData = teams[data.team]
+                    if (teamData.coins - data.amount >= 0 && teamData.bank + data.amount >= 0) {
+                        status = 200
+                        teamData.coins -= data.amount
+                        teamData.bank += data.amount
+                    } else status = 400
+                    teams[data.team] = teamData
+                } else status = 401
+                Bun.write("teams.json", JSON.stringify(teams))
+                return Response.json(teamData, { status });
+            }
+            if (url.pathname === "/claim") {
+                const data = await req.text().then(JSON.parse)
+                readJSON("teams.json", teams => {
+                    if (data.team in teams) {
+                        readJSON("challenges.json", challenges => {
+                            // Close the challenge
+                            const challenge = challenges.locational.concat(challenges.nonlocational)
+                                .filter(v => v.title === data.challenge)[0]
+                            challenge.open = false
+                            Bun.write("challenges.json", JSON.stringify(challenges))
 
-                        // Update team coins
-                        editJSON("teams.json", teams => {
-                            teams[data.team].coins += challenge.reward
-                            return teams
+                            // Update team coins
+                            editJSON("teams.json", teams => {
+                                teams[data.team].coins += challenge.reward
+                                return teams
+                            })
                         })
-                    })
-                    return new Response("Claim succesful!", { status: 200 })
-                } else {
-                    return new Response("Team invalid, redirecting...", { status: 401 })
-                }
+                        return new Response("Claim succesful!", { status: 200 })
+                    } else {
+                        return new Response("Team invalid, redirecting...", { status: 401 })
+                    }
+                })
             }
         }
 
