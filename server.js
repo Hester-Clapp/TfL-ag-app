261e7bfcff8493d3a9ec2ef8d95a6ddd54c27c67
Hester Clapp <18hclapp@cms.tela.org.uk>
2024-07-31 16:10:24 +0100

    persistent teams (not working)

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-const teams = {}
-// const sessions = {}
-
 const pages = {
     "/": "./index.html",
     "/login": "./login.html",
@@ -9,6 +6,7 @@ const pages = {
     "/steal": "./steal.html",
     "/style": "./style.css",
     "/challenges.json": "./challenges.json",
+    "/teams.json": "./teams.json",
 }
 
 const server = Bun.serve({
@@ -18,25 +16,53 @@ const server = Bun.serve({
         if (req.method === "POST") {
             if (url.pathname === "/login") {
                 const team = await req.text()
-                if (!(team in teams)) teams[team] = {
-                    coins: 1000,
-                    bank: 0,
-                }
-                console.log(teams)
-
+                editJSON("teams.json", teams => {
+                    teams[team] = teams?.[team] || {
+                        coins: 1000,
+                        bank: 0,
+                    }
+                    return teams
+                })
                 return Response.json({ team });
             }
-            if (url.pathname === "/data") {
+            if (url.pathname === "/teamdata") {
                 const team = await req.text()
-                return Response.json(teams[team] || {})
+                readJSON("teams.json", teams => {
+                    return Response.json(teams[team] || {})
+                })
+            }
+            if (url.pathname === "/claim") {
+                const data = await req.text().then(text => JSON.parse(text))
+                if (data.team in teams) {
+                    readJSON("challenges.json", challenges => {
+                        // Close the challenge
+                        const challenge = challenges.locational.concat(challenges.nonlocational)
+                            .filter(v => v.title === data.challenge)[0]
+                        challenge.open = false
+                        Bun.write("challenges.json", JSON.stringify(challenges))
+
+                        // Update team coins
+                        editJSON("teams.json", teams => {
+                            teams[data.team].coins += challenge.reward
+                            return teams
+                        })
+                    })
+                    return new Response("Claim succesful!", { status: 200 })
+                } else {
+                    return new Response("Team invalid, redirecting...", { status: 401 })
+                }
             }
         }
 
         if (req.method === "GET" && url.pathname in pages) {
+            console.log(url.pathname)
             return new Response(Bun.file(pages[url.pathname]))
         }
 
         return new Response("Page not found", { status: 404 });
     }
-});
+})
 console.log(`Server running on port ${server.port}`)
+
+async function readJSON(path, then) { Bun.file(path).json().then(then) }
+async function editJSON(path, edit) { Bun.file(path).json().then(json => { Bun.write(path, edit(json)) }) }
