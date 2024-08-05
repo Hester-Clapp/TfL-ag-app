68634599468b9544b1084a60bf8500deb5ec3d88
Hester Clapp <18hclapp@cms.tela.org.uk>
2024-08-05 18:53:41 +0100

    steal purchasing

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const pages = {
     "/deposit": "./deposit.html",
     "/challenges": "./challenges.html",
     "/steal": "./steal.html",
+    "/shop": "./stealshop.html",
     "/style": "./style.css",
-    "/challenges.json": "./challenges.json",
+    "/log.json": "./log.json",
     "/teams.json": "./teams.json",
+    "/challenges.json": "./challenges.json",
+    "/steals.json": "./steals.json",
 }
 
 const server = Bun.serve({
@@ -17,23 +20,22 @@ const server = Bun.serve({
             console.log(`Receiving POST request to ${url.pathname}`)
             if (url.pathname === "/login") {
                 const team = await req.text()
-                console.log(`Adding ${team} to database`)
-                editJSON("teams.json", teams => {
-                    if (!(team in teams)) {
-                        teams[team] = {
-                            coins: 1000,
-                            bank: 0,
-                        }
+                const teams = await open("teams")
+                if (!(team in teams)) {
+                    console.log(`Adding ${team} to database`)
+                    teams[team] = {
+                        coins: 1000,
+                        bank: 0,
                     }
-                    console.log(teams)
-                    return teams
-                })
+                    write("teams", teams)
+                    logEvent(`${team} has joined`)
+                }
                 return Response.json({ team }, { status: 201 });
             }
             if (url.pathname === "/deposit") {
                 const data = await req.text().then(JSON.parse)
+                const teams = await open("teams")
                 let status, teamData
-                const teams = await Bun.file("teams.json").json()
                 if (data.team in teams) {
                     teamData = teams[data.team]
                     if (teamData.coins - data.amount >= 0 && teamData.bank + data.amount >= 0) {
@@ -43,31 +45,51 @@ const server = Bun.serve({
                     } else status = 400
                     teams[data.team] = teamData
                 } else status = 401
-                Bun.write("teams.json", JSON.stringify(teams))
+                write("teams", teams)
+                logEvent(`${data.team} deposited money at the bank`)
                 return Response.json(teamData, { status });
             }
             if (url.pathname === "/claim") {
                 const data = await req.text().then(JSON.parse)
-                readJSON("teams.json", teams => {
-                    if (data.team in teams) {
-                        readJSON("challenges.json", challenges => {
-                            // Close the challenge
-                            const challenge = challenges.locational.concat(challenges.nonlocational)
-                                .filter(v => v.title === data.challenge)[0]
-                            challenge.open = false
-                            Bun.write("challenges.json", JSON.stringify(challenges))
+                const teams = await open("teams")
+                const challenges = await open("challenges")
+                if (data.team in teams) {
 
-                            // Update team coins
-                            editJSON("teams.json", teams => {
-                                teams[data.team].coins += challenge.reward
-                                return teams
-                            })
-                        })
-                        return new Response("Claim succesful!", { status: 200 })
-                    } else {
-                        return new Response("Team invalid, redirecting...", { status: 401 })
-                    }
-                })
+                    // Close the challenge
+                    const challenge = challenges.locational.concat(challenges.nonlocational)
+                        .filter(v => v.title === data.challenge)[0]
+                    challenge.open = false
+                    write("challenges", challenges)
+
+                    // Update team coins
+                    teams[data.team].coins += challenge.reward
+                    write("teams", teams)
+                    logEvent(`${data.team} claimed the challenge '${data.challenge}'`)
+                    return new Response("Claim succesful!", { status: 200 })
+                } else {
+                    return new Response("Team invalid, redirecting...", { status: 401 })
+                }
+            }
+            if (url.pathname === "/purchase_token") {
+                const data = await req.text().then(JSON.parse)
+                const teams = await open("teams")
+                const steals = await open("steals")
+                if (data.team in teams) {
+                    const steal = steals.filter(v => v.title === data.steal)[0]
+
+                    // Update team coins
+                    teams[data.team].coins -= steal.cost
+                    write("teams", teams)
+
+                    // Change the owner of the steal token
+                    steal.owner = data.team
+                    write("steals", steals)
+                    logEvent(`${data.team} purchased the steal token '${data.steal}'`)
+                    return new Response("Purchase succesful!", { status: 200 })
+                } else {
+                    return new Response("Team invalid, redirecting...", { status: 401 })
+                }
+                // })
             }
         }
 
@@ -81,5 +103,6 @@ const server = Bun.serve({
 })
 console.log(`Server running on port ${server.port}`)
 
-async function readJSON(path, then) { Bun.file(path).json().then(then) }
-async function editJSON(path, edit) { Bun.file(path).json().then(json => { Bun.write(path, JSON.stringify(edit(json))) }) }
+function open(path) { return Bun.file(`./${path}.json`).json() }
+function write(path, data) { Bun.write(`./${path}.json`, JSON.stringify(data)) }
+function logEvent(event) { open("log").then(log => [`${new Date().toTimeString().slice(0, 5)}: ${event}`].concat(log)).then(log => write("log", log)) }
